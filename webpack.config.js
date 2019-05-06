03b4f137c68a253fdc8d14860fcf7040fed68bde
mlawry <mlawry@gmail.com>
2019-05-06 10:18:24 +1000

    Added sass bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -19,5 +19,39 @@ module.exports = [
       filename: 'index_bundle.js',
       path: __dirname + '/dist'
     }
+  },
+  {
+    entry: './app/index.scss',
+    devtool: false,
+    mode: "development",
+    module: {
+      rules: [{
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'index_bundle.css'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [ './node_modules' ]
+            }
+          }
+        ]
+      }]
+    },
+    output: {
+      // This is necessary for webpack to compile. We never use ignore-style-bundle.js
+      filename: 'ignore-style-bundle.js',
+      path: __dirname + '/dist'
+    }
   }
 ];
